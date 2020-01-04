1d3436d69adef258713349c3ed7c75b7bd352cf9
Dipayan Sen Gupta <dip.himu@gmail.com>
2020-01-04 17:53:49 +0600

    Add hot load

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 function App() {
   const user = {
     firstName: 'Dipayan',
